5351642fbde73a9faa15ccf25d1f7d9744748ad5
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-02-21 15:42:21 +0100

    Added error notices for empty or invalid search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,11 @@
 class Stock < ApplicationRecord
-
-    def self.new_lookup(ticker_symbol)
-client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-                                    endpoint: 'https://sandbox.iexapis.com/v1')
-          
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-
-    end 
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    begin
+        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      nil
+      end
+  end
 end
