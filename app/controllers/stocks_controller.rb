b032e85f17c32f1a29f7b8b736fa3e3275826c68
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-02-22 14:43:19 +0100

    Completed functionality for looking up stock, using ajax to submit form remotely, and handling errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,22 @@
-class StocksController < ApplicationController 
-
-    def  search
-        if params[:stock].present? 
-            @stock = Stock.new_lookup(params[:stock])
-            if @stock
-                respond_to do |format|
-                    format.js { render partial: 'users/result' }
-                end 
-            else
-                flash[:alert] = "Please enter valid symbol to search"
-                redirect_to my_portfolio_path
-            end
-        else
-            flash[:alert] = "Please enter a symbol to search"
-            redirect_to my_portfolio_path
+class StocksController < ApplicationController
+  def search
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
+      else
+        respond_to do |format|
+            flash.now[:alert] = 'Please enter valid symbol to search'
+            format.js { render partial: 'users/result' }
+        end 
+      end
+    else
+        respond_to do |format|
+            flash.now[:alert] = 'Please enter a symbol to search'
+            format.js { render partial: 'users/result' }
         end
     end
+  end
 end
