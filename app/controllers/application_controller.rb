0f420cb2e270e27734183bdf44f96d1a79a28481
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-03-02 14:50:03 +0100

    Fixed nil class error with current user changed messages partial embedded ruby code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :current_user, :logged_in?, :alert_color
- 
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-   end
+  protected
 
-  def logged_in?
-    !!current_user
-   end
-
-    def require_user
-      if !logged_in?
-        flash[:alert] = 'you must be logged in to perform that action'
-        redirect_to login_path
-    end
-  end
-
-  def alert_color(name)
-   if name == "notice"
-      return "alert alert-success alert-dismissible fade show"
-    else 
-      return "alert alert-danger alert-dismissible fade show"
-    end
-  end
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+end  
 
 end
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  
+ 
+  
   def my_portfolio
+    @user = current_user
+    @tracked_stocks = current_user.stocks
   end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end 
+
 end
+
+
+
