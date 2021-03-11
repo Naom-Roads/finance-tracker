ec687df4f0da97948935b8a20c011f5060ea3791
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-03-11 14:42:01 +0100

    Added destroy option for stocks and fixed layout issue

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,4 +9,14 @@ class UserStocksController < ApplicationController
     flash[:notice] = "#{stock.name} was successfully added to your portfolio"
     redirect_to my_portfolio_path
   end
+
+def destroy 
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy 
+    flash[:notice] = "#{stock.ticker} was removed from portfolio"
+    redirect_to my_portfolio_path 
+
+end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root 'dashboard#index'
   get 'welcome', to: 'dashboard#index'
