019227054e1423d537f4b4aa916a958386829cd4
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-05-14 11:51:00 +0200

    Update on friends list

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,8 +9,8 @@ class Stock < ApplicationRecord
                                   endpoint: 'https://sandbox.iexapis.com/v1')
     begin
         new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-    rescue => exception
-      return nil
+    rescue StandardError => e
+      nil
       end
   end
 
