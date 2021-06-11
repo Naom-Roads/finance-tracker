37d742555bef00ab3bd3c2014959c9b9a56878bb
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-06-11 16:38:12 +0200

    Removed application mailer settings

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,12 +1,12 @@
 class ApplicationMailer < ActionMailer::Base 
 
-  ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.delivery_method = :mailjet
 
-  ActionMailer::Base.sendmail_settings = { :address => "smtp.gmail.com", :port => "25", :domain => "gmail.com",
-  :user_name => "nrrodrig@gmail.com", :password => "138mArA88!!", :authentication => "plain", :enable_starttls_auto => true }
+#   ActionMailer::Base.sendmail_settings = { :address => "in-v3.mailjet.com", :port => "587", :domain => "gmail.com",
+#   :user_name => "nrrodrig@gmail.com", :password => "138mArA88!!", :authentication => "plain", :enable_starttls_auto => true }
 
-  default from: 'nrrodrig@gmail.com'
-  layout 'mailer'
+#   default from: 'nrrodrig@gmail.com'
+#   layout 'mailer'
   
 end
 
