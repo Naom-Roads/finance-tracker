60c3224795ec4a00bfb281cf9d8e31e5db9741c2
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-06-11 12:50:32 +0200

    Readded Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
 end
